3800cd1a0c0a3fcd382d86c10a84209ba4f47553
mauricekollewe <kollewem.35@gmail.com>
2022-01-04 17:23:59 +0000

    create Cards, Charts and CountrySelector folders

--- covid-19-app/src/App.js
+++ covid-19-app/src/App.js
@@ -1,15 +1,35 @@
-import React from 'react'
-// import { render } from 'react-dom'
+import React, { useState, useEffect } from 'react';
+import { Cards, Chart, CountrySelector } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
 
-class App extends React.Component {
-  render() {
+
+const App = () => {
+  const [url, setUrl] = useState()
+
+  useEffect(() => {
+    console.log('USE')
+    const getData = async () => {
+      console.log('Try')
+      try {
+        const data = await fetchData()
+        setUrl(data)
+        console.log('DATA', data)
+      } catch (error) {
+        console.log('Error', error)
+      }
+    }
+    getData()
+  }, []);
+
     return (
-      <div>
-        <h1>App</h1>
+      <div className={styles.container}>
+        <Cards data={url} />
+        <CountrySelector />
+        <Chart />
       </div>
-    )
-  }
+    );
 }
 
-export default App
+export default App;
 
---
+++ covid-19-app/src/components/CountrySelector/CountrySelector.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CountrySelector = () => {
+	return (
+		<div>
+			<h1>CountrySelector</h1>
+		</div>
+	)
+}
+
+export default CountrySelector;
