337217438dbe150505f3c11d3151d004dcddad18
mauricekollewe <kollewem.35@gmail.com>
2022-01-04 18:24:35 +0000

    update Cards

--- covid-19-app/src/components/Cards/Cards.jsx
+++ covid-19-app/src/components/Cards/Cards.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+// import { Card, CardContent, Typography, Grid } from '@material-ui/icons';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+
+
+import styles from './Cards.module.css';
 
 const Cards = (props) => {
 	console.log(props);
 
 	return (
-		<div>
+		<div className={styles.container}>
+			<CardContent>
+				<Typography gutterBottom variant="h5" component="h2">
+					Infected
+				</Typography>
+				<Typography variant="body2" color="textSecondary" component="h2">
+					Real Data
+				</Typography>
+				<Typography gutterBottom variant="h5" component="h2">
+					Real Date
+				</Typography>
+				<Typography variant="body2" color="textSecondary" component="h2">
+					Number of active cases of COVID-19
+				</Typography>
+			</CardContent>
 			<h1>Cards</h1>
 		</div>
 	)
